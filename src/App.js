ff92301c18c27e6468d9b5f23b5889cff9603736
daeuk heo <gjeodnr2@gmail.com>
2023-05-26 16:09:46 +0900

    20230526

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Routes, Route} from 'react-router-dom'
 import FireStoreTest from './page/FireStoreTest';
+import Login from './page/Login';
 
 function App() {
   return (
     <div>
       <Routes>
+      <Route path='/login' element={<Login />} />
         <Route path='/' element={<FireStoreTest/>} />
       </Routes>
     </div>
